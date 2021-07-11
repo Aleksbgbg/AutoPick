0146053d40f1292f2abb1e3b19c94194864eb4e6
Aleksbgbg <kozichka01@abv.bg>
2021-07-11 23:03:17 +0100

    Tidy up UtilityRunner unit tests and disable by default.

--- AutoPick.Tests/UtilityRunner.cs
+++ AutoPick.Tests/UtilityRunner.cs
@@ -2,287 +2,175 @@
 {
     using System;
     using System.Drawing;
-    using System.Runtime.InteropServices;
-    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoPick.Win32.Native;
     using Emgu.CV;
     using Emgu.CV.CvEnum;
     using Emgu.CV.OCR;
     using Emgu.CV.Structure;
     using Xunit;
-    using Xunit.Abstractions;
 
+    // Utilities to help sourcing screenshots and testing features.
+    // Ideally these utilities should be made into a utility program.
+    // All tests here are disabled to prevent running by default.
+    // All tests are parameterised with example parameters - configure for your specific use case.
+    // To run a test, enable it and ensure the test case(s) use your required parameters.
     public class UtilityRunner
     {
-        private readonly ITestOutputHelper _testOutputHelper;
+        private const string SkipReason = "All utilities skipped by default.";
 
-        public UtilityRunner(ITestOutputHelper testOutputHelper)
+        [Theory(Skip = SkipReason)]
+        [InlineData(@"A:\Downloads\tessdata-master", @"A:\Text.png", "Hi")]
+        public void TestTesseract(string tesseractDataDirectory, string textImagePath, string expectedResult)
         {
-            _testOutputHelper = testOutputHelper;
+            Tesseract tesseract = new(tesseractDataDirectory, "eng", OcrEngineMode.Default);
+            tesseract.SetImage(new Image<Rgb, byte>(textImagePath));
+
+            string discoveredText = tesseract.GetOsdText();
+
+            Assert.Equal(expectedResult, discoveredText);
         }
 
-        public enum State
+        [Theory(Skip = SkipReason)]
+        [InlineData(@"A:\Content\League\Connecting.png",
+                    @"A:\Content\League\Capture\Image1.png",
+                    @"A:\Content\League\Capture\match-Image1.png",
+                    32, 644, 72, 10)]
+        public void MatchImageAndSaveMatchRegionToNewImage(string templatePath, string imageToMatchPath, string outputPath,
+                                                           int matchX, int matchY, int matchWidth, int matchHeight)
         {
-            Default,
-            Lobby,
-            Queue,
-            Accept,
-            Decline,
-            Connecting,
-            Pick,
-            Selected,
-            Locked
-        }
+            Image<Gray, byte> template = new(templatePath);
+            Image<Gray, byte> matchRegion = new Image<Gray, byte>(imageToMatchPath)
+                .GetSubRect(new Rectangle(matchX, matchY, matchWidth, matchHeight));
 
-        [DllImport("Kernel32.dll")]
-        private static extern int GetLastError();
+            Image<Gray, float> matchImage = matchRegion.MatchTemplate(template, TemplateMatchingType.CcoeffNormed);
+            double minVal = 0;
+            double maxVal = 0;
+            Point minLoc = Point.Empty;
+            Point maxLoc = Point.Empty;
+            CvInvoke.MinMaxLoc(matchImage, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            public int x;
-            public int y;
-        }
-
-        [DllImport("User32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindowA(string lpClassName, string lpWindowName);
-
-        [DllImport("User32.dll")]
-        private static extern IntPtr GetDC(IntPtr window);
-
-        [DllImport("Gdi32.dll")]
-        private static extern IntPtr CreateCompatibleDC(IntPtr deviceContext);
-
-        [DllImport("User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetWindowRect(IntPtr window, out RECT rect);
-
-        [DllImport("Gdi32.dll")]
-        private static extern IntPtr CreateCompatibleBitmap(IntPtr deviceContext, int x, int y);
-
-        [DllImport("Gdi32.dll")]
-        private static extern IntPtr SelectObject(IntPtr deviceContext, IntPtr bitmap);
-
-        [DllImport("User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool PrintWindow(IntPtr window, IntPtr deviceContext, uint flags);
-
-        [DllImport("User32.dll")]
-        private static extern int ReleaseDC(IntPtr window, IntPtr deviceContext);
-
-        [DllImport("Gdi32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool DeleteDC(IntPtr deviceContext);
-
-        [DllImport("Gdi32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool DeleteObject(IntPtr obj);
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="image"></param>
-        /// <param name="expectedState"></param>
-        [DllImport("User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool ClientToScreen(IntPtr window, out POINT point);
-
-        [DllImport("User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        // [DllImport("User32.dll")]
-        // private static extern uint SendInput(
-        //     uint nInputs,
-        //     ref INPUT pInputs,
-        //     // [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs,
-        //     int cbSize);
-
-        [DllImport("User32.dll")]
-        private static extern int GetSystemMetrics(int nIndex);
-
-        [Fact]
-        public void Tesseract()
-        {
-            // string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\";
-            var _ocr = new Tesseract("A:\\Downloads\\tessdata-master\\", "eng", OcrEngineMode.Default);
-            var i = new Image<Rgb, byte>("A:\\Text.png");
-            _ocr.SetImage(i);
-            string z = _ocr.GetOsdText();
-            // var result = .GetCharacters();
-            Assert.Equal("hi", _ocr.GetOsdText());
-        }
-
-        [Fact]
-        public void Match()
-        {
-            var template = new Image<Gray, byte>("A:\\Connecting.png");
-            var p = new Rectangle(32, 644, 72, 10);
-
-            for (int i = 41; i < 67; ++i)
+            float[,,] matches = matchImage.Data;
+            for (int y = 0; y < matches.GetLength(0); y++)
             {
-                var s = new Image<Gray, byte>($"A:\\Content\\League\\Capture\\Image{i}.png").GetSubRect(
-                    new Rectangle(p.X, p.Y, p.Width, p.Height));
-
-                var r = s.MatchTemplate(template, TemplateMatchingType.CcoeffNormed);
-                double minVal = 0;
-                double maxVal = 0;
-                Point minLoc = Point.Empty;
-                Point maxLoc = Point.Empty;
-                CvInvoke.MinMaxLoc(r, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
-
-                float[,,] matches = r.Data;
-                for (int y = 0; y < matches.GetLength(0); y++)
+                for (int x = 0; x < matches.GetLength(1); x++)
                 {
-                    for (int x = 0; x < matches.GetLength(1); x++)
+                    float matchScore = matches[y, x, 0];
+
+                    if (matchScore > 0.5f)
                     {
-                        double matchScore = matches[y, x, 0];
-
-                        _testOutputHelper.WriteLine(matchScore.ToString());
-
-                        if (matchScore > 0.5)
-                        {
-                            s.Draw(new Rectangle(x + 1, y + 1, p.Width, p.Height), new Gray(1), 0);
-                        }
-
+                        matchRegion.Draw(new Rectangle(x - 1, y - 1, matchWidth + 1, matchHeight + 1), new Gray(1));
                     }
-
                 }
 
-                // _testOutputHelper.WriteLine(PixelsMatch(s, template, p).ToString());
-                // _testOutputHelper.WriteLine(string.Join(", ", minVal, maxVal, $"[{minLoc.X}, {minLoc.Y}], [{maxLoc.X}, {maxLoc.Y}]"));
-
-
-                s.Save($"A:\\Content\\League\\Capture\\result-Image{i}.png");
             }
+
+            matchRegion.Save(outputPath);
         }
 
-        [Theory]
-        [InlineData("[4]Accept.png", "Accept.png", 646, 383, 107, 20)]
-        [InlineData("[5]Accepted.png", "Accepted.png", 619, 383, 119, 20)]
-        [InlineData("[5]Decline.png", "Declined.png", 623, 383, 129, 20)]
-        public void CutImage(string image, string name, int x, int y, int w, int h)
+        [Theory(Skip = SkipReason)]
+        [InlineData(@"A:\Programming\AutoPick\AutoPick\Detect\Accept.png",
+                    @"A:\Content\League\[4]Accept.png",
+                    646, 383, 107, 20)]
+        public void CutImage(string sourceImage, string outputPath, int matchX, int matchY, int width, int height)
         {
-            int width = w;
-            int height = h;
-
+            Image<Gray, byte> source = new(sourceImage);
             Image<Gray, byte> target = new(new Size(width, height));
 
-            Image<Gray, byte> source = new($"A:\\Content\\League\\{image}");
-
-            // source.MatchTemplate(target, )
-
-            for (int xs = 0; xs < w; ++xs)
+            for (int x = 0; x < width; ++x)
             {
-                for (int ys = 0; ys < h; ++ys)
+                for (int y = 0; y < height; ++y)
                 {
-                    target[new Point(xs, ys)] = source[new Point(xs + x, ys + y)];
+                    target[new Point(x, y)] = source[new Point(x + matchX, y + matchY)];
                 }
             }
 
-            target.Save($"A:\\Programming\\AutoPick\\AutoPick\\Detect\\{name}");
+            target.Save(outputPath);
         }
 
-        [Fact]
-        public void ScreenshotClient()
+        [Theory(Skip = SkipReason)]
+        [InlineData(0, true, @"A:\Content\League\Capture")]
+        public async Task ScreenshotClient(int delayBeforeScreenshotMs, bool infinite, string outputDirectory)
         {
-            IntPtr window = FindWindowA(null, "League of Legends");
+            IntPtr window = Win32Util.FindWindowA(null, "League of Legends");
 
-            IntPtr sourceDeviceContext = GetDC(window);
-            IntPtr targetDeviceContext = CreateCompatibleDC(sourceDeviceContext);
+            IntPtr sourceDeviceContext = Win32Util.GetDC(window);
+            IntPtr targetDeviceContext = Win32Util.CreateCompatibleDC(sourceDeviceContext);
 
-            RECT windowRect = new();
-            GetWindowRect(window, out windowRect);
+            Win32Util.GetWindowRect(window, out Win32Rect windowRect);
+            IntPtr bitmap = Win32Util.CreateCompatibleBitmap(sourceDeviceContext, windowRect.Width, windowRect.Height);
+            Win32Util.SelectObject(targetDeviceContext, bitmap);
 
-            IntPtr bitmap = CreateCompatibleBitmap
-            (
-                sourceDeviceContext,
-                windowRect.Right - windowRect.Left,
-                windowRect.Bottom - windowRect.Top
-            );
-            SelectObject(targetDeviceContext, bitmap);
+            await Task.Delay(delayBeforeScreenshotMs);
 
-            int i = 0;
-
-            Thread.Sleep(3000);
-
-            while (i++ == 0)
+            for (int i = 0; infinite; ++i)
             {
-                PrintWindow(window, targetDeviceContext, 1 /* PW_CLIENTONLY */);
-
-                Image.FromHbitmap(bitmap).ToImage<Rgb, byte>().Save($"A:\\Content\\League\\Capture\\Image{i++}.png");
+                Win32Util.PrintWindow(window, targetDeviceContext, PrintWindowParam.PW_CLIENTONLY);
+                Image.FromHbitmap(bitmap).ToImage<Rgb, byte>().Save($@"{outputDirectory}\Image{i++}.png");
             }
 
-            ReleaseDC(window, sourceDeviceContext);
-            DeleteDC(targetDeviceContext);
-            DeleteObject(bitmap);
+            Win32Util.DeleteObject(bitmap);
+            Win32Util.DeleteDC(targetDeviceContext);
+            Win32Util.ReleaseDC(window, sourceDeviceContext);
         }
 
-        [Fact]
-        public void ExtractFramesFromVideo()
+        [Theory(Skip = SkipReason)]
+        [InlineData(@"B:\Videos\07-04 19-55-35.avi", @"A:\Content\League\Extract")]
+        public void ExtractFramesFromVideo(string videoPath, string outputDirectory)
         {
-            var v = new VideoCapture(@"B:\Videos\07-04 19-55-35.avi", VideoCapture.API.Ffmpeg);
+            VideoCapture videoCapture = new(videoPath, VideoCapture.API.Ffmpeg);
 
-            int i = 0;
-            while (++i > 0)
+            for (int i = 0; ; ++i)
             {
-                var f = v.QueryFrame();
-                if (f == null)
+                Mat frame = videoCapture.QueryFrame();
+
+                if (frame == null)
                 {
                     break;
                 }
 
-                f.Save($@"A:\Content\League\Extract\{i}.png");
+                frame.Save($@"{outputDirectory}\{i}.png");
             }
         }
 
-        [Fact]
-
-        public void XD()
+        [Theory(Skip = SkipReason)]
+        [InlineData(
+            @"A:\Programming\AutoPick\AutoPick.Tests\bin\Debug\net5.0-windows\TestImages\1280x720\BasicScreens\[7]Connecting.png",
+            @"A:\Programming\AutoPick\AutoPick.Tests\bin\Debug\net5.0-windows\TestImages\1920x1080\ConnectingTransitionFrames\1692.png",
+            @"A:\ResizeComparison.png",
+            28, 640, 80, 20)]
+        public void CompareRegionBeforeAndAfterResize(string originalImagePath, string imageToResizePath,
+                                                      string outputPath,
+                                                      int regionX, int regionY, int regionWidth, int regionHeight)
         {
-            var small =new Image<Rgb, byte>(@"A:\Programming\AutoPick\AutoPick.Tests\bin\Debug\net5.0-windows\TestImages\1280x720\BasicScreens\[7]Connecting.png");
-            var large =new Image<Rgb, byte>(@"A:\Programming\AutoPick\AutoPick.Tests\bin\Debug\net5.0-windows\TestImages\1920x1080\ConnectingTransitionFrames\1692.png");
-            var target = new Rectangle(28, 640, 80, 20);
+            Image<Rgb, byte> originalImage = new(originalImagePath);
+            Image<Rgb, byte> imageToResize = new(imageToResizePath);
+            Rectangle targetRegion = new(regionX, regionY, regionWidth, regionHeight);
 
-            var save = new Image<Rgb, byte>(target.Width, (target.Height * 2) + 1);
+            Image<Rgb, byte> comparisonOutputImage = new(targetRegion.Width, (targetRegion.Height * 2) + 1);
 
-           // small.GetSubRect(target).Copy(save, mask);
+            Image<Rgb, byte> subregion = originalImage.GetSubRect(targetRegion);
+            for (int x = 0; x < targetRegion.Width; ++x)
+            {
+                for (int y = 0; y < targetRegion.Height; ++y)
+                {
+                    comparisonOutputImage[y, x] = subregion[y, x];
+                }
+            }
 
-           var subr = small.GetSubRect(target);
+            subregion = imageToResize.Resize(1280, 720, Inter.Lanczos4).GetSubRect(targetRegion);
+            for (int y = targetRegion.Height + 1; y < ((targetRegion.Height * 2) + 1); ++y)
+            {
+                for (int x = 0; x < (targetRegion.Width); ++x)
+                {
+                    comparisonOutputImage[y, x] = subregion[y - (targetRegion.Height + 1), x];
+                }
+            }
 
-           for (int x = 0; x < target.Width; ++x)
-           {
-               for (int y = 0; y < (target.Height); ++y)
-               {
-                   save[y, x] = subr[y, x];
-               }
-           }
-
-           subr = large.Resize(1280, 720, Inter.Linear).GetSubRect(target);
-
-           for (int y = ((target.Height) + 1); y < ((target.Height * 2) + 1); ++y)
-           {
-               for (int x = 0; x < (target.Width); ++x)
-               {
-                   save[y, x] = subr[y - ((target.Height) + 1), x];
-               }
-           }
-
-           save.Draw(new LineSegment2D(new Point(0, target.Height), new Point(target.Width, target.Height)), new Rgb(Color.Red), 1);
-
-
-
-                    // .Resize(1280, 720, Inter.Lanczos4);
-                     save.Save("A:\\Resized3.png");
+            comparisonOutputImage.Draw(new LineSegment2D(new Point(0, targetRegion.Height),
+                                                         new Point(targetRegion.Width, targetRegion.Height)),
+                                       new Rgb(Color.Red), 1);
+            comparisonOutputImage.Save(outputPath);
         }
-
     }
 }
--- README.md
+++ README.md
@@ -7,5 +7,7 @@ Currently only works on 1280x720 clients and the UI is not very advanced - impro
 
 If the program misbehaves, it can be disabled by pressing `Ctrl+Shift+Alt+F2` and re-enabled by pressing `Ctrl+Shift+Alt+F3`.
 
+To get a release version you can run, right click `publish.ps1` and press `Run with PowerShell`, or execute `powershell ./publish.ps1` in the current directory. The script will produce a runnable executable `AutoPick.exe`.
+
 ## Demo
 ![AutoPick demo](demo.gif)
